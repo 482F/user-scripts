21b57de18bfe5f9e5ce778758793c662597690d6
482F <0ryonryoko0@gmail.com>
2021-11-13 20:04:35 +0900

    ページを自動遷移するスクリプトを追加

---
+++ mhw-antenna-auto-shift.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         mhアンテナ自動遷移
+// @namespace    482F
+// @version      0.1
+// @author       482F
+// @match        http://mhwmatome.antenam.jp/items/view/*
+// @grant        unsafeWindow
+// @require      file://C:/user-scripts/mhw-antenna-auto-shift.js
+// ==/UserScript==
+
+;(async function () {
+  const f = unsafeWindow.tmFunctions
+  location.href =
+    'http://mhwmatome.antenam.jp/items/click/' +
+    location.href.match(/[^\/]+$/)[0]
+})()
